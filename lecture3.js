763be9b64fdcfffe1044d8e0bbcd73cd161cbe78
Sukalpa Devkota <143915953+sukalpa0@users.noreply.github.com>
2026-04-29 07:34:34 +0545

    Add files via upload
    Lecture-3 Learning JS Loops and Strings

---
+++ lecture3.js
@@ -0,0 +1,94 @@
+
+
+// for (i = 1; i<=10; i++){
+//     console.log("Sukalpa Devkota");
+// }
+
+// let num = prompt("Enter a number");
+// let sum = 0;
+
+// for (let i = 1; i <= num) {
+//     sum += i;
+// }
+
+// console.log(sum);
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+
+// let name = "Sukalpa";
+
+// console.log(name.length);
+// console.log(name.length - 1);
+// console.log(name[name.length - 1]);
+
+// let name = "sukalpa";
+
+// for (let i = 0; i < name.length; i++){
+//     console.log(name[i]);
+// }
+
+// let name = "sukalpa";
+// for (let i=0; i<name.length; i++) {
+//     if (name[i] !== 'a') {
+//     console.log(name[i]);
+//     }
+// }
+
+
+// let name = "sukalpa devkota";
+// let count = 0;
+
+// for (let i = 0; i <= name.length; i++){
+//     if (name[i] == 'a') {
+//         count += 1;
+//     }
+// }
+// console.log(count); //outside the loop because it prints only the final answer.
+
+// let name = "sukalpa";
+// let reversed = "";
+
+// for (let i=name.length - 1; i>=0; i--) {
+//     reversed += name[i];
+// }
+// console.log(reversed);
+
+
+
+// let name = "sukalpa devkota";
+// count = 0;
+// for (let i = 0; i < name.length; i++) {
+//     if (name[i] !== 'a' && name[i] !=='e' && name[i] !=='i' && name[i] !=='o' && name[i] !=='u'&& name[i] !==' '){
+//         count += 1;
+//     }
+// }
+// console.log(count);
+
+// let name = "javascript";
+// let reverse = "";
+// for (let i = name.length - 1; i >= 0; i--){
+//     if(name[i] !== 'a' && name[i] !=='e' && name[i] !=='i' && name[i] !=='o' && name[i] !=='u'){
+//         reverse += name[i];
+//     }
+// }
+// console.log(reverse);
+
+
+// PALINDROME SOLVEDDDDDD
+
+// let palindrome = "madam";
+// reverse = "";
+ 
+// for (let i = palindrome.length - 1; i >= 0; i--) {
+//     reverse += palindrome[i]; 
+// }
+// if (palindrome === reverse) {
+//     console.log("True");
+//     } else {
+//         console.log("False");
+//     }
+
